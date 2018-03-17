7062232a14c49ebd2056b56a3e7046423a65a7c7
tonkoshkurik <rapyo@ukr.net>
2018-03-17 20:39:37 +0200

    gulp

--- js/main.js
+++ js/main.js
@@ -7,4 +7,7 @@ $(function() {
     $('.close').on('click',function() {
     $('nav.menu-overlay').toggleClass('open');
     });
+    $('.cats-toggle').on('click', function() {
+        $('.blog-header .blog-header-wrapper .menu-flex').fadeToggle();
+    });
 });
